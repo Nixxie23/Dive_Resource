083eb5782f327fbfdba80b71bba8f80e7b275052
taylor Schuchter <taylorschuchter@gmail.com>
2023-06-15 14:26:20 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dive_Resources.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
